bd7b2a1dcc6d513a74cfc7f66d7bd2124e2dd9f9
Fábio Gaspar <fabiodanielreis@hotmail.com>
2020-02-09 18:26:11 +0000

    Add support for sleep time argument

--- scripts/kbd_sleep.sh
+++ scripts/kbd_sleep.sh
@@ -3,7 +3,15 @@
 KBD_MASTER_ID=3
 KBD_SLAVE_ID=15
 
+if [[ $# -eq 1 ]]; then
+	# Retrieve the sleep time provided by user
+	SLEEP_TIME=$1
+else
+	# if no argument is provided, then default to 120 seconds
+	SLEEP_TIME=120
+fi
+
 # Disable the keyboard slave by removing it from the master device
 xinput --float $KBD_SLAVE_ID
 # Re-add the keyboard slave to the master keyboard, thus enabling it
-sleep 5 ; xinput --reattach $KBD_SLAVE_ID $KBD_MASTER_ID
+sleep $SLEEP_TIME ; xinput --reattach $KBD_SLAVE_ID $KBD_MASTER_ID
