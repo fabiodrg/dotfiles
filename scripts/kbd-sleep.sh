1a3431c30ca5a615e5e276ea1140f9b7e3e48f64
Fabio Gaspar <fabiodanielreis@hotmail.com>
2021-10-03 00:00:29 +0100

    Refactor script for disabling/enabling keyboard device
    - Automatically get the keyboard device identifier in the X.org system
    - Support device toggling, by automatically determining wether the device is enabled or not, and flip the status
    - Keep both operating modes: toggling (default), disable temporarially for a user-defined amount of time

--- scripts/kbd-sleep.sh
+++ scripts/kbd-sleep.sh
@@ -1,20 +1,53 @@
-# How long the keyboard should be disabled (seconds)
-SLEEP_TIME=120
-
-# Parse user argument, if any
-if [[ $# -eq 1 ]]; then
-	# Override default sleep time with user input time
-	SLEEP_TIME=$1
-fi
-
 # Get the device ID for the keyboard
-KBD_ID=$(xinput list --id-only 'AT Translated Set 2 keyboard')
+DEVICE_NAME='AT Translated Set 2 keyboard'
+KBD_ID=$(xinput list --id-only "$DEVICE_NAME")
 
-# Disable the keyboard
-xinput disable $KBD_ID
+# Enable the keyboard device
+function kbd_enable() {
+	xinput enable $KBD_ID
+	notify-send -t 0 \
+		-i input-keyboard-virtual-on \
+		-c "device.added" \
+		"Keyboard enabled" \
+		"Run script to toggle the keyboard state and disable it"
+}
 
-# Waiting period
-sleep $SLEEP_TIME
+# Disable the keyboard device
+function kbd_disable() {
+	xinput disable $KBD_ID
+	notify-send -t 0 \
+		-i input-keyboard-virtual-off \
+		-c "device.removed" \
+		"Keyboard disabled" \
+		"Run script to toggle the keyboard state and enable it"
 
-# Re-enable the keyboard device
-xinput enable $KBD_ID
+}
+
+# Get current keyboard state, i.e. enabled or disabled
+function kbd_get_state() {
+	local state=$(xinput list-props "$DEVICE_NAME" | grep "Device Enabled" | awk '{ print $4 }')
+	echo $state
+}
+
+# Temporarially disable the keyboard for a period of time
+function kbd_tmp_sleep() {
+	kbd_disable
+	sleep $1
+	kbd_enable
+}
+
+# Toggle the keyboard state, i.e. if it is disabled then enable it and vice versa
+function kbd_state_toggle() {
+	if [ $(kbd_get_state) -eq 1 ]; then
+		kbd_disable
+	else
+		kbd_enable
+	fi
+}
+
+# If has single argument, assume its the elapse time
+if [[ $# -eq 1 ]]; then
+	kbd_tmp_sleep $1
+else
+	kbd_state_toggle
+fi
